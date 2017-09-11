7000650054faef9bb36a37c68ec2880b1659d2f3
mcnichol <mcnichol.m@gmail.com>
2017-09-11 09:23:01 -0500

    Base React Application setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
-module.exports = {
-  entry: './dev/app.jsx',
-  output: {
-    path: path.resolve('dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
-  }
-}
+
+const APP_DIR = path.resolve("dev/app.jsx");
+const BUILD_DIR = path.resolve("dist");
+
+const config = {
+    entry: APP_DIR,
+    output: {
+        path: BUILD_DIR,
+        filename: 'js/bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                "presets": ["es2015", "react"]
+            }
+        }]
+    }
+};
+
+module.exports = config;
